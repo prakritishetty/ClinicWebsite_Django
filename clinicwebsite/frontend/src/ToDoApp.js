e2a5048137442bc42834da427ae8e6b8f990c47b
PrakritiShetty <prakritishetty02@gmail.com>
2022-11-30 12:46:10 +0530

    to-do app completed

--- clinicwebsite/frontend/src/ToDoApp.js
+++ clinicwebsite/frontend/src/ToDoApp.js
@@ -1,5 +1,7 @@
 import React, {Component} from "react"
 
+import Modal from './components/ToDoModal'
+
 const todoItems = [ 
 { 
   id: 1,
@@ -37,9 +39,38 @@ class App extends Component{
 
       viewCompleted: false,
       todoList: todoItems,
+      modal: false,
+      activeItem: {title:"", description:"", completed: false}
     };
 
   }
+  toggle = ()=>{
+    this.setState({
+      modal: !this.state.modal
+    });
+  };
+
+  handleDelete = (item) => {
+    this.toggle();
+
+    alert("delete" + JSON.stringify(item));
+  };
+  handleSubmit = (item) => {
+    this.toggle();
+
+    alert("save" + JSON.stringify(item));
+  };
+  createItem = () =>{
+    const item = { title:"", description:"", completed:false };
+
+    this.setState({activeItem: item, modal: !this.state.modal});
+
+  }
+  editItem = (item) =>{
+    this.setState({activeItem: item, modal: !this.state.modal});
+    
+  }
+
 
   displayCompleted = (status) => {
     if(status)
@@ -80,10 +111,10 @@ class App extends Component{
           {item.title}
         </span>
         <span>
-          <button className = "btn btn-secondary mr-2">
+          <button className = "btn btn-secondary mr-2" onClick={ ()=>this.editItem(item)}>
             Edit
           </button>
-          <button className="btn btn-danger">
+          <button className="btn btn-danger" onClick={ ()=>this.handleDelete(item)}>
             Delete
           </button>
         </span>
@@ -101,7 +132,7 @@ class App extends Component{
           <div className = "col-md-6 col-sm-10 mx-auto p-0">
             <div className = "card p-3">
               <div className = "mb-4">
-                <button className = "btn btn-primary">
+                <button className = "btn btn-primary" onClick={ ()=>this.createItem}>
                   Add task
                 </button>
               </div>
@@ -112,6 +143,7 @@ class App extends Component{
             </div>
           </div>
         </div>
+        {this.state.modal ? <Modal activeItem={this.state.activeItem} toggle={this.toggle} onSave={this.handleSubmit} /> : null}
       </main>
     );
   }
